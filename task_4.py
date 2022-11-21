23b6f790f00a163575e6ab24d8fcbaf78623d6cf
Alexandr Volkovich <volkovich.a@edu.spbstu.ru>
2022-11-21 17:00:17 +0300

    Исправил замечания

--- task_4.py
+++ task_4.py
@@ -1,22 +1,22 @@
 import json
+from pprint import pprint
 
 INPUT_FILE = "input.csv"
+OUTPUT_FILE = "output.json"
 
 
 def csv_to_list_dict(filename, delimeter=",", new_line="\n") -> list[dict]:
     with open(filename, "r") as file:
-        data = file.readlines()
-        # Разделяем заголовки от данных
-        headers = data[0].replace(new_line, "").split(delimeter)  # убираем знак новой строки и создаем список элементов
-        lines_of_values = data[1:]
-        # Создаем будущий список всех обьектов
-        list_of_objects = []
-        # Каждая строчка - это набор параемтров отдельного объекта
-        for line in lines_of_values:
-            object_values = line.replace(new_line, "").split(delimeter)  # убираем знак новой строки и создаем список элементов
-            list_of_objects.append(dict(zip(headers, object_values)))
-
-        return list_of_objects
+        header = file.readline() # read first row
+        headers = header.replace(new_line, "").split(delimeter)
+        
+        for line in file:
+            object_values = line.replace(new_line, "").split(delimeter)
+            yield dict(zip(headers, object_values))
 
 
-print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
+def dict_printer(line):
+    pprint([record for record in line])
+
+
+dict_printer(csv_to_list_dict(INPUT_FILE))
