0066f6400219a3f1b4050bcc4d3ee6c5d0c3ff76
Alexandr Volkovich <volkovich.a@edu.spbstu.ru>
2023-03-09 13:25:29 +0300

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,9 +1,9 @@
-src = not False and True or False and not True
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# TODO расписать упрощение выражения
-# result = True and True or False and False  # избавляемся от отрицаний
-# result = True or False  # избавляемся от логического "И"
-# result = True  # избавляемся от логического "ИЛИ"
-result = True
+# TODO Оформить решение
+# Находим индекс максимального элемента
+max_index = list_numbers.index(max(list_numbers))
+#Меняем местами
+list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_index]
 
-print(src == result)
+print(list_numbers)
